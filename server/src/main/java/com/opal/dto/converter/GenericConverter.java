782d82773494f46f8d4a475c982a722481c6ac54
Oleksandr Palamarchuk <alex.y.palamarchuk@gmail.com>
2020-12-29 18:30:59 +0200

    adding dto package

---
+++ server/src/main/java/com/opal/dto/converter/GenericConverter.java
@@ -0,0 +1,26 @@
+package com.opal.dto.converter;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Function;
+
+import static java.util.stream.Collectors.toList;
+
+public interface GenericConverter<I, O> extends Function<I, O> {
+
+    default O convert(final I input) {
+        O output = null;
+        if (input != null) {
+            output = this.apply(input);
+        }
+        return output;
+    }
+
+    default List<O> convert(final List<I> input) {
+        List<O> output = new ArrayList<O>();
+        if (input != null) {
+            output = input.stream().map(this::apply).collect(toList());
+        }
+        return output;
+    }
+}
